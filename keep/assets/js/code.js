1c0ea09bff4c6253e6014eb890764eaa246874e8
Miguel Nieto <migustanieto.2@gmail.com>
2023-04-17 20:00:23 -0500

    keep 3 parte 2

--- keep/assets/js/code.js
+++ keep/assets/js/code.js
@@ -8,14 +8,12 @@ function guardarNota() {
         tituloNota.classList.add("border", "border-danger")
         textoNota.classList.add("border", "border-danger")
     } else {
-        debugger
         let cantidadNotasActuales = 1
         if (localStorage.getItem("cantidad") == null) {
             localStorage.setItem("cantidad", cantidadNotasActuales)
         } else {
             cantidadNotasActuales = localStorage.getItem("cantidad")
-            cantidadNotasActuales = parseInt(cantidadNotasActuales)
-            cantidadNotasActuales = cantidadNotasActuales + 1
+            cantidadNotasActuales = parseInt(cantidadNotasActuales) + 1
             localStorage.setItem("cantidad", cantidadNotasActuales)
         }
         localStorage.setItem("titulo" + cantidadNotasActuales, tituloNota.value)
@@ -27,22 +25,33 @@ function guardarNota() {
 }
 
 function mostrarNotas() {
+    let cantidadNotasActuales = localStorage.getItem("cantidad")
     let divNotas = document.querySelector("#notasGuardadas")
-
-    let tituloNota = localStorage.getItem("titulo")
-    let nota = localStorage.getItem("nota")
-
-    divNotas.innerHTML = `
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${tituloNota}</h5>
-                    <p class="card-text">${nota}</p>
+    if (cantidadNotasActuales == null) {
+        divNotas.innerHTML = "<h1>No hay ninguna nota</h1>"
+    } else {
+        divNotas.innerHTML = ''
+        for (let x = 1; x <= cantidadNotasActuales; x++) {
+            let tituloNota = localStorage.getItem(`titulo${x}`)
+            let nota = localStorage.getItem(`nota${x}`)
+            divNotas.innerHTML += `
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${tituloNota}</h5>
+                        <p class="card-text">${nota}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    `
+        `
+        }
+    }
+
+
+
+
 }
+
 
 
 function disparaAlerta(mensajeDeLaAlerta, icono, colorIcono, colorTexto) {
