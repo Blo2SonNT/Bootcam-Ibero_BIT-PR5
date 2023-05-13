0aac44ca43f9e8e2ec6e389123eb231bace311d1
Miguel Nieto <migustanieto.2@gmail.com>
2023-05-12 19:56:17 -0500

    consultar, consultar especifico y eliminar

--- backend1/controllers/mascotasController.js
+++ backend1/controllers/mascotasController.js
@@ -11,3 +11,59 @@ exports.crearMascota = async(req, res) => {
         res.status(500).send('Comuníquese con el administrador')
     }
 }
+
+
+exports.obtenerRegistrosMascotas = async(req, res) => {
+    try {
+        let mascota = await Mascotas.find()
+        res.json(mascota)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Comuníquese con el administrador')
+    }
+}
+
+exports.actualizarRegistroMascota = async(req, res) => {
+
+}
+
+
+exports.eliminarRegistroMascota = async(req, res) => {
+    try {
+        let regexIdMongo = /^[0-9a-fA-F]{24}$/
+        if (regexIdMongo.test(req.params.id)) {
+            let data_mascota = await Mascotas.findById(req.params.id)
+            if (!data_mascota) {
+                res.status(404).send('Mascota no encontrada')
+            } else {
+                await Mascotas.findOneAndRemove({ _id: req.params.id })
+                res.status(200).send('Registro de mascota eliminada correctamente')
+            }
+        } else {
+            res.status(400).send('El identificador de la mascota no es correcto')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Comuníquese con el administrador')
+    }
+}
+
+
+exports.obtenerRegistro = async(req, res) => {
+    try {
+        let regexIdMongo = /^[0-9a-fA-F]{24}$/
+        if (regexIdMongo.test(req.params.id)) {
+            let data_mascota = await Mascotas.findById(req.params.id)
+            if (!data_mascota) {
+                res.status(404).send('Mascota no encontrada')
+            } else {
+                res.json(data_mascota)
+            }
+        } else {
+            res.status(400).send('El identificador de la mascota no es correcto')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Comuníquese con el administrador')
+    }
+}
--- backend1/routes/routes.js
+++ backend1/routes/routes.js
@@ -3,18 +3,14 @@ const router = express.Router()
 const mascotaController = require('../controllers/mascotasController')
 
 
-router.get('/', (req, res) => {
-    res.send('Me manda datos la api')
-})
-
-router.put('/', (req, res) => {
-    res.send('Actualizando datos de mi api')
-})
-
-router.delete('/', (req, res) => {
-    res.send('Borrando datos de mi api')
-})
-
+router.get('/', mascotaController.obtenerRegistrosMascotas)
 router.post('/', mascotaController.crearMascota)
+
+router.put('/:id', mascotaController.actualizarRegistroMascota)
+
+
+router.delete('/:id', mascotaController.eliminarRegistroMascota)
+
+router.get('/:id', mascotaController.obtenerRegistro)
 
 module.exports = router
