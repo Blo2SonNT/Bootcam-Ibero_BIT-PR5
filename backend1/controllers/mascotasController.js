58653c1e0bbec18f4377b4cb1952de65d110e3e9
Miguel Nieto <migustanieto.2@gmail.com>
2023-05-16 19:41:54 -0500

    API REST - CRUS

--- backend1/controllers/mascotasController.js
+++ backend1/controllers/mascotasController.js
@@ -24,7 +24,30 @@ exports.obtenerRegistrosMascotas = async(req, res) => {
 }
 
 exports.actualizarRegistroMascota = async(req, res) => {
+    try {
+        let regexIdMongo = /^[0-9a-fA-F]{24}$/
+        if (regexIdMongo.test(req.params.id)) {
+            let data_mascota = await Mascotas.findById(req.params.id)
+            if (!data_mascota) {
+                res.status(404).send('Mascota no encontrada')
+            }
 
+            const { nombre, edad, propietario, doc_propietario, url_imagen } = req.body
+            data_mascota.nombre = nombre
+            data_mascota.edad = edad
+            data_mascota.propietario = propietario
+            data_mascota.doc_propietario = doc_propietario
+            data_mascota.url_imagen = url_imagen
+
+            data_mascota = await Mascotas.findOneAndUpdate({ _id: req.params.id }, data_mascota, { new: true })
+            res.json(data_mascota)
+        } else {
+            res.status(400).send('El identificador de la mascota no es correcto')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Comuníquese con el administrador')
+    }
 }
 
 
